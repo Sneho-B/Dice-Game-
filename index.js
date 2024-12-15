55c3f0dbaed67a9cd030a23bc393c2b600e99982
Snehoritro Bose <snehobjs1749@gmail.com>
2024-12-15 22:31:43 +0530

    This is a simple dice game made using html css and js

---
+++ index.js
@@ -0,0 +1,15 @@
+var randomNumber1= Math.floor(Math.random()*6)+1;
+var randomNumber2= Math.floor(Math.random()*6)+1;
+var diceImage1= "images/dice"+randomNumber1+".png";
+var diceImage2= "images/dice"+randomNumber2+".png";
+document.querySelectorAll(".img1")[0].setAttribute("src",diceImage1);
+document.querySelectorAll(".img2")[0].setAttribute("src",diceImage2);
+if (randomNumber1> randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Wins";
+}
+else if ( randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 Wins";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw";
+}
